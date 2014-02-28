db8c5958bc95545ed1cc9a42a8cfcafe22e1a275
Jon Arild Torresdal <jon@torresdal.net>
2014-02-28 09:21:50 +0100

    Trying again...

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jdunn@opscode.com"
 license          "MIT"
 description      "Installs and configures Couchbase Server."
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "1.1.16"
+version          "1.1.17"
 
 %w{debian ubuntu centos redhat oracle amazon scientific windows}.each do |os|
   supports os
