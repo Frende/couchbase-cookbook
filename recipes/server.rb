b8e825cda9f4b7a4ee7a10d7e4ebd2386aa8a382
Jon Arild Torresdal <jon@torresdal.net>
2014-03-03 14:48:03 +0100

    trying again...

--- recipes/server.rb
+++ recipes/server.rb
@@ -154,7 +154,9 @@ if node['fqdn'].casecmp("#{node['couchbase']['server']['cluster-init-server']}")
   end
 else
   batch 'Add node to CouchBase cluster and rebalance' do
-    code "\"#{node['couchbase']['server']['cli_path']}\" rebalance -c #{node['couchbase']['server']['cluster-init-server']}:#{node['couchbase']['server']['port']} --server-add=#{node['fqdn']} -u \"#{node['couchbase']['server']['username']}\" -p \"#{node['couchbase']['server']['password']}\""
+    code <<-EOH
+      "#{node['couchbase']['server']['cli_path']}" rebalance -c #{node['couchbase']['server']['cluster-init-server']}:#{node['couchbase']['server']['port']} --server-add=#{node['fqdn']} -u "#{node['couchbase']['server']['username']}" -p "#{node['couchbase']['server']['password']}"
+    EOH
     not_if {allready_configured}
   end
 end
